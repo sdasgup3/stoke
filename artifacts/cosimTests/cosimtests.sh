3e763af1338fa3ea86bcbd27f26a65947cf6ab26
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-03-05 12:08:48 -0600

    make memeory stores visible to solver

--- artifacts/cosimTests/cosimtests.sh
+++ artifacts/cosimTests/cosimtests.sh
@@ -5,8 +5,8 @@ STRATA_CHECK_SAT_CIRCUIT=$HOME/Github/strata/stoke/bin/stoke_check_circuit_with_
 PROCESSPL=$HOME/Github/binary-decompilation/x86-semantics/scripts/process_spec.pl
 
 # Create concrete instances
-#grep imm ../extra_support_needs_tobe_added_memory.txt 2>&1 > /tmp/imm_instrs
-#grep -v imm ../extra_support_needs_tobe_added_memory.txt 2>&1 >/tmp/non_imm_instrs
+grep imm ../extra_support_needs_tobe_added_memory.txt 2>&1 > /tmp/imm_instrs
+grep -v imm ../extra_support_needs_tobe_added_memory.txt 2>&1 >/tmp/non_imm_instrs
 #
 #cat ../extra_support_needs_tobe_added_registers.txt  | parallel "$PROCESSPL --prepare_concrete      --opcode {} --workdir concrete_instances/register-variants/{}"
 #
@@ -19,12 +19,16 @@ PROCESSPL=$HOME/Github/binary-decompilation/x86-semantics/scripts/process_spec.p
 
 # Update testcases for memory instructions
 #cat ../extra_support_needs_tobe_added_memory.txt | parallel "$PROCESSPL --opcode  {}  --instructions_path concrete_instances/memory-variants/{}/instructions/ --update_tc --testid 00"
+#cat  /tmp/non_imm_instrs | parallel "$PROCESSPL --opcode  {}  --instructions_path concrete_instances/memory-variants/{}/instructions/ --update_tc --testid 00"
+#cat  /tmp/imm_instrs | parallel "$PROCESSPL --opcode  {}  --instructions_path concrete_instances/memory-variants/{}/instructions/ --update_tc --testid 00"
 
 # Register Tests
 #cat ../extra_support_needs_tobe_added_registers.txt | parallel "$PROCESSPL --check_stoke --file concrete_instances/register-variants/{}/check_stoke.txt --instructions_path concrete_instances/register-variants/{}/instructions  --testid 00 --stokedir /home/sdasgup3/Github/develop_stoke/ --no_strata_handler" &> runlog.00.reg.txt
 
 # Immediates Tests
-cat ../extra_support_needs_tobe_added_immediates.txt | parallel "$PROCESSPL  --check_stoke --file concrete_instances/immediate-variants/{}/check_stoke.txt --instructions_path concrete_instances/immediate-variants/{}/instructions  --testid 00 --stokedir /home/sdasgup3/Github/develop_stoke/ --no_strata_handler" 
+#cat ../extra_support_needs_tobe_added_immediates.txt | parallel "$PROCESSPL  --check_stoke --file concrete_instances/immediate-variants/{}/check_stoke.txt --instructions_path concrete_instances/immediate-variants/{}/instructions  --testid 00 --stokedir /home/sdasgup3/Github/develop_stoke/ --no_strata_handler" 
 
 # Memory Tests
 #cat ../extra_support_needs_tobe_added_memory.txt | parallel "$PROCESSPL --check_stoke --file concrete_instances/memory-variants/{}/check_stoke.txt --instructions_path concrete_instances/memory-variants/{}/instructions --use_updated_tc --testid 02 --stokedir /home/sdasgup3/Github/develop_stoke/ --no_strata_handler"
+#cat  /tmp/non_imm_instrs | parallel -j 1 "$PROCESSPL --check_stoke --file concrete_instances/memory-variants/{}/check_stoke.txt --instructions_path concrete_instances/memory-variants/{}/instructions --use_updated_tc --testid 00 --stokedir /home/sdasgup3/Github/develop_stoke/ --no_strata_handler"
+cat  /tmp/imm_instrs | parallel -j 1 "$PROCESSPL --check_stoke --file concrete_instances/memory-variants/{}/check_stoke.txt --instructions_path concrete_instances/memory-variants/{}/instructions --use_updated_tc --testid 00 --stokedir /home/sdasgup3/Github/develop_stoke/ --no_strata_handler"
