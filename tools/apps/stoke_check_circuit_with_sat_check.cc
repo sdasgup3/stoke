65197d03239de76fd4d38aae5cf1d36f482a40dc
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-03-07 16:02:55 -0600

    Fixing the semantis of cmpxchg and shufb based on fuzz testing results.     The bug appears when the memory effectve address involves %rax, which is same as     the accumulator to be modifed by the instruction. The ordering of destination update     and accumulator update needs to be corrected to avoid reads from a location specified by     the updated value of %rax.

--- tools/apps/stoke_check_circuit_with_sat_check.cc
+++ tools/apps/stoke_check_circuit_with_sat_check.cc
@@ -12,25 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cassert>
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <random>
 #include <map>
+#include <random>
 #include <string>
 #include <vector>
-#include <cassert>
 
 #include "src/ext/cpputil/include/command_line/command_line.h"
-#include "src/ext/cpputil/include/signal/debug_handler.h"
-#include "src/ext/cpputil/include/io/filterstream.h"
 #include "src/ext/cpputil/include/io/column.h"
 #include "src/ext/cpputil/include/io/console.h"
+#include "src/ext/cpputil/include/io/filterstream.h"
+#include "src/ext/cpputil/include/signal/debug_handler.h"
 
 #include "src/ext/x64asm/src/reg_set.h"
 
-#include "src/symstate/simplify.h"
 #include "src/symstate/pretty_visitor.h"
+#include "src/symstate/simplify.h"
 
 #include "src/validator/bounded.h"
 #include "src/validator/handler.h"
@@ -56,7 +56,7 @@ using namespace stoke;
 using namespace x64asm;
 using namespace boost;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
   target_arg.required(true);
   rewrite_arg.required(false);
@@ -78,7 +78,7 @@ int main(int argc, char** argv) {
   SandboxGadget sb(test_set, aux_fxns);
 
   // Run all the testcases using the cfg `target`
-  cout << "Run " << test_set.size() <<   " tests\n";
+  cout << "Run " << test_set.size() << " tests\n";
   sb.insert_function(target);
   sb.set_entrypoint(target.get_code()[0].get_operand<x64asm::Label>(0));
   sb.run();
@@ -99,40 +99,76 @@ int main(int argc, char** argv) {
 
   auto k = sb.input_begin();
   auto ke = sb.input_end();
-  for (auto i = sb.result_begin(), ie = sb.result_end(); i != ie && k != ke; ++i, ++k) {
+  for (auto i = sb.result_begin(), ie = sb.result_end(); i != ie && k != ke;
+       ++i, ++k) {
     CpuState ss = *i;
     if (ss.code != stoke::ErrorCode::NORMAL) {
-      cout<< "Sandbox did not finish normally: " << opcode << " " <<  (int)ss.code << std::endl;
+      cout << "Sandbox did not finish normally: " << opcode << " "
+           << (int)ss.code << std::endl;
       exit(4);
-      //reference_out_.push_back(*k); // for dov/idiv we can push the input value as the result. Also their semantics is also written so that on error state the output value is same as input value
+      // reference_out_.push_back(*k); // for dov/idiv we can push the input
+      // value as the result. Also their semantics is also written so that on
+      // error state the output value is same as input value
     } else {
       reference_out_.push_back(*i);
     }
   }
 
   // Build the sym formula of the circuit
-  //cout << "Build Strata Combo Handler\n";
+  // cout << "Build Strata Combo Handler\n";
   // ComboHandler ch("/home/sdasgup3/Github/strata-data/circuits/");
   ComboHandler ch;
-  //ComboHandler ch(strata_path_arg.value());
+  // ComboHandler ch(strata_path_arg.value());
 
   if (ch.get_support(instr) == Handler::SupportLevel::NONE) {
-    //cout << "\033[1;31mNot supported\033[0m\n";
-    cout << "Not Supported (" <<  opcode << ")\n";
+    // cout << "\033[1;31mNot supported\033[0m\n";
+    cout << "Not Supported (" << opcode << ")\n";
     exit(4);
   }
 
   // check equivalence of two symbolic states for a given register
-  auto is_eq = [&](auto& reg, auto a, auto b, stringstream& explanation, auto& cs) {
+  auto is_eq = [&](string name, auto a, auto b, stringstream &explanation,
+                   auto &cs, vector<SymBool> constraints_a,
+                   vector<SymBool> constraints_b) {
     // SymBool eq = a == b;
-    // At times simplification is requires as the sym to z3 transformer might crash.
+    // At times simplification is requires as the sym to z3 transformer might
+    // crash.
     SymBool eq = SymSimplify().simplify(a) == SymSimplify().simplify(b);
-    //cout << eq << "\n";
+    vector<SymBool> eqs = {eq};
+    // cout << eq << "\n";
 
-    //cout <<  "\n\n" << *reg << ":\n";
-    // SImplified
-    //cout << SymSimplify().simplify(a) << "\n";
-    //cout << SymSimplify().simplify(b) << "\n\n\n";
+    cout << "\n\n" << name << ":\n";
+    // Printing
+    auto simplify = true;
+    cout << SymSimplify().simplify(a) << "\n";
+    cout << SymSimplify().simplify(b) << "\n\n\n";
+    if (name == "memory") {
+      for (auto &c : constraints_a) {
+        stringstream ss;
+        ss << SymSimplify().simplify(c);
+        cout << " Consraint a:     " << ss.str() << endl;
+      }
+      for (auto &c : constraints_b) {
+        stringstream ss;
+        ss << SymSimplify().simplify(c);
+        cout << " Consraint b:     " << ss.str() << endl;
+      }
+    }
+
+    for (auto &c : constraints_a) {
+      if (simplify) {
+        eqs.push_back(SymSimplify().simplify(c));
+      } else {
+        eqs.push_back(c);
+      }
+    }
+    for (auto &c : constraints_b) {
+      if (simplify) {
+        eqs.push_back(SymSimplify().simplify(c));
+      } else {
+        eqs.push_back(c);
+      }
+    }
 
     // Not simplified
     // cout << a << "\n";
@@ -140,38 +176,49 @@ int main(int argc, char** argv) {
 
     /*
     cout << "Simplified a" << "\n";
-    cout << solver.getZ3Formula(SymSimplify().simplify(a)) << std::flush << "\n";
+    cout << solver.getZ3Formula(SymSimplify().simplify(a)) << std::flush <<
+    "\n";
     cout << "Simplified b" << "\n";
-    cout << solver.getZ3Formula(SymSimplify().simplify(b)) << std::flush << "\n";
+    cout << solver.getZ3Formula(SymSimplify().simplify(b)) << std::flush <<
+    "\n";
     cout << "Non-Simplified a" << "\n";
     cout << solver.getZ3Formula(a) << "\n";
     cout << "Non-Simplified b" << "\n";
     cout << solver.getZ3Formula(b) << "\n";
     */
 
-    //bool res = solver.is_sat({ !eq });
-    bool res = solver.is_sat({ eq });
+    // bool res = solver.is_sat({ !eq });
+    bool res = solver.is_sat({eqs});
     if (solver.has_error()) {
-      explanation << "  solver encountered error: " << solver.get_error() << endl;
+      explanation << "  solver encountered error: " << solver.get_error()
+                  << endl;
       return false;
     }
 
-    //if (res) {
+    // if (res) {
     if (!res) {
       cout << cs << "\n";
       cout << "\n\n";
 
-      explanation << "  states do not agree for '" << (*reg) << "':" << endl;
-      auto simplify = true;
-      /*
+      explanation << "  states do not agree for '" << name << "':" << endl;
       if (!simplify) {
         explanation << "    validator: " << (a) << endl;
       } else {
         explanation << "    validator: " << SymSimplify().simplify(a) << endl;
       }
-      */
-      explanation << "    validator: " << solver.getZ3Formula(SymSimplify().simplify(a)) << endl;
-      explanation << "    sandbox:   " << solver.getZ3Formula(SymSimplify().simplify(b)) << endl;
+      explanation << "    sandbox:   " << b << endl;
+      if (name == "memory") {
+        for (auto &c : constraints_a) {
+          stringstream ss;
+          ss << SymSimplify().simplify(c);
+          explanation << "      " << ss.str() << endl;
+        }
+      }
+
+      // explanation << "    validator: " <<
+      // solver.getZ3Formula(SymSimplify().simplify(a)) << endl;
+      // explanation << "    sandbox:   " <<
+      // solver.getZ3Formula(SymSimplify().simplify(b)) << endl;
       return false;
     } else {
       return true;
@@ -187,7 +234,8 @@ int main(int argc, char** argv) {
   if (instr.get_opcode() == CALL_LABEL) {
     // more precise dataflow information from annotations
     for (const auto& fxn : aux_fxns) {
-      if (instr.get_operand<Label>(0) == fxn.get_code()[0].get_operand<Label>(0)) {
+      if (instr.get_operand<Label>(0) ==
+  fxn.get_code()[0].get_operand<Label>(0)) {
         rs = rs - fxn.get_may_must_sets().maybe_undef_set;
       }
     }
@@ -199,16 +247,27 @@ int main(int argc, char** argv) {
   // Just the live_outs
   auto rs = live_out_arg.value();
   auto maybe_undef_rs = maybe_undef_out_arg.value();
+  auto to_string = [](auto &reg) -> string {
+    stringstream ss;
+    ss << (*reg);
+    return ss.str();
+  };
 
-  //Iterate on each testcase
+  auto get_memory = [](auto &state) {
+    return (((FlatMemory *)state.memory)->heap_);
+  };
+  auto get_constraints = [](auto &state) -> vector<SymBool> {
+    return (((FlatMemory *)state.memory)->constraints_);
+  };
+
+  // Iterate on each testcase
   cout << "Check Equivalence\n";
   int i = 0;
   RegSet *dummy = new RegSet();
   int count = 0;
 
-  for (const auto& cs : test_set) {
-    //std::cout << count++ << std::flush << "\n";
-
+  for (const auto &cs : test_set) {
+    // std::cout << count++ << std::flush << "\n";
 
     // Create a formula with initial state as the test input
 
@@ -225,41 +284,53 @@ int main(int argc, char** argv) {
     // Do equivalence check
     auto eq = true;
     stringstream ss;
-    ss << "Sandbox and validator do not agree for '" << instr << "' (opcode " << opcode << ")" << endl;
+    ss << "Sandbox and validator do not agree for '" << instr << "' (opcode "
+       << opcode << ")" << endl;
     for (auto gp_it = rs.gp_begin(); gp_it != rs.gp_end(); ++gp_it) {
-      eq = eq && is_eq(gp_it, sym_validator.lookup(*gp_it), sb_validator.lookup(*gp_it), ss, cs);
+      eq = eq && is_eq(to_string(gp_it), sym_validator.lookup(*gp_it),
+                       sb_validator.lookup(*gp_it), ss, cs, {}, {});
     }
     for (auto sse_it = rs.sse_begin(); sse_it != rs.sse_end(); ++sse_it) {
-      eq = eq && is_eq(sse_it, sym_validator.lookup(*sse_it), sb_validator.lookup(*sse_it), ss, cs);
+      eq = eq && is_eq(to_string(sse_it), sym_validator.lookup(*sse_it),
+                       sb_validator.lookup(*sse_it), ss, cs, {}, {});
     }
-    for (auto flag_it = rs.flags_begin(); flag_it != rs.flags_end(); ++flag_it) {
-      eq = eq && is_eq(flag_it, sym_validator[*flag_it], sb_validator[*flag_it], ss, cs);
+    for (auto flag_it = rs.flags_begin(); flag_it != rs.flags_end();
+         ++flag_it) {
+      eq = eq && is_eq(to_string(flag_it), sym_validator[*flag_it],
+                       sb_validator[*flag_it], ss, cs, {}, {});
     }
 
     // Check Memory writes
     if (check_mem_writes) {
       auto mem_op = instr.get_operand<M8>(instr.mem_index());
-      eq = eq && is_eq(dummy, sym_validator.lookup(mem_op), sb_validator.lookup(mem_op), ss, cs);
+      // eq = eq && is_eq("memory", sym_validator.lookup(mem_op),
+      // sb_validator.lookup(mem_op), ss, cs);
+      eq = eq &&
+           is_eq("memory", get_memory(sym_validator), get_memory(sb_validator),
+                 ss, cs, get_constraints(sym_validator),
+                 get_constraints(sb_validator));
     }
     if (!eq) {
       cout << ss.str() << endl;
     }
-
 
     /* Test for maybe undefs
     auto eq_undef = true;
     stringstream ss_undef;
     ss_undef << "[Undef Test] Sandbox and validator do not agree for '" << instr
              << "' (opcode " << opcode << ")" << endl;
-    for (auto gp_it = maybe_undef_rs.gp_begin(); gp_it != maybe_undef_rs.gp_end(); ++gp_it) {
+    for (auto gp_it = maybe_undef_rs.gp_begin(); gp_it !=
+    maybe_undef_rs.gp_end(); ++gp_it) {
       eq_undef = eq_undef && is_eq(gp_it, sym_validator.lookup(*gp_it),
                                    sb_validator.lookup(*gp_it), ss_undef, cs);
     }
-    for (auto sse_it = maybe_undef_rs.sse_begin(); sse_it != maybe_undef_rs.sse_end(); ++sse_it) {
+    for (auto sse_it = maybe_undef_rs.sse_begin(); sse_it !=
+    maybe_undef_rs.sse_end(); ++sse_it) {
       eq_undef = eq_undef && is_eq(sse_it, sym_validator.lookup(*sse_it),
                                    sb_validator.lookup(*sse_it), ss_undef, cs);
     }
-    for (auto flag_it = maybe_undef_rs.flags_begin(); flag_it != maybe_undef_rs.flags_end(); ++flag_it) {
+    for (auto flag_it = maybe_undef_rs.flags_begin(); flag_it !=
+    maybe_undef_rs.flags_end(); ++flag_it) {
       eq_undef = eq_undef && is_eq(flag_it, sym_validator[*flag_it],
                                    sb_validator[*flag_it], ss_undef, cs);
     }
@@ -273,10 +344,9 @@ int main(int argc, char** argv) {
     }
     */
 
-
     i++;
-    if (i%1000 == 0 ) {
-      cout << "Completed " <<i << "cases" <<  endl;
+    if (i % 1000 == 0) {
+      cout << "Completed " << i << "cases" << endl;
     }
 
     sym_validator.clearSymRegs();
@@ -287,6 +357,3 @@ int main(int argc, char** argv) {
 
   return 0;
 }
-
-
-
