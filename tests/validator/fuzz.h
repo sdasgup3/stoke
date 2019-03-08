ef42a94368cecd8717f5164d528569ea252a6f6e
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-03-08 16:59:59 -0600

    refactoring of fuzz tester

--- tests/validator/fuzz.h
+++ tests/validator/fuzz.h
@@ -493,18 +493,18 @@ TEST_F(ValidatorFuzzTest, RandomInstructionRandomState) {
   tp.remove_opcode(MUL_M64);
 
   // code to use if you want to test a single instruction only
-  /*
   for (size_t i = 0; i < X64ASM_NUM_OPCODES; ++i) {
     tp.remove_opcode((x64asm::Opcode)i);
   }
+  /*
   tp.insert_opcode(x64asm::Opcode::PALIGNR_XMM_XMM_IMM8);
   tp.insert_opcode(x64asm::Opcode::VPALIGNR_XMM_XMM_XMM_IMM8);
   tp.insert_opcode(x64asm::Opcode::VPALIGNR_YMM_YMM_YMM_IMM8);
   tp.insert_opcode(x64asm::Opcode::PALIGNR_XMM_M128_IMM8);
   tp.insert_opcode(x64asm::Opcode::VPALIGNR_XMM_XMM_M128_IMM8);
   tp.insert_opcode(x64asm::Opcode::VPALIGNR_YMM_YMM_M256_IMM8);
-#include"instrs_under_test.inc"
   */
+#include"instrs_under_test.inc"
 
 
 
